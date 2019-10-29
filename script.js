a3367560c41a1c06358ee5d49b1ebfe143faf1bc
Liam Deppong <liamdepp0605@gmail.com>
2019-10-29 19:08:59 -0400

    Changed to random number generator

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function vibeCheck (vibes){
+/*function vibeCheck (vibes){
     if(vibes === "true"){
         document.querySelector("#displayVibe").innerText = "You have Passed the Vibe check!"
         document.querySelector("#displayVibe").style.color = "green";
@@ -15,4 +15,16 @@ form.addEventListener("submit", function(e){
     vibeCheck(
         formData.get("vibes"));
     form.reset(); 
+}); */
+
+let button = document.querySelector("button");
+button.addEventListener("click", function(){
+    let randomVibe = Math.floor(Math.random() * 100);
+    if (randomVibe < 50){
+        document.querySelector("#displayVibe").innerText = "You have passed the Vibe check!"
+        document.querySelector("#displayVibe").style.color = "green";
+    } else {
+        document.querySelector("#displayVibe").innerText = "Vibe's off bro, check yourself.";
+        document.querySelector("#displayVibe").style.color = "red";
+    }
 });
